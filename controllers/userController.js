bbd30539f496fa010706bfa160f481d0efe47433
Robert Lopez <robertlopez@Roberts-MBP.lan>
2022-04-06 15:00:33 -0700

    update route

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,7 +67,7 @@ const getUser = asyncHandler(async (req, res) => {
 
 // Generate JWT
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
     expiresIn: "30d",
   });
 };
