0b000ec77c1b161a31e66e444b6870683d00a613
Robert Lopez <robertlopez@Roberts-MBP.lan>
2022-04-06 10:04:45 -0700

    installed heroku

--- connection.js
+++ connection.js
@@ -1,5 +1,20 @@
-const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1/buzz-kill-mern-api")
+const mongoose = require("mongoose");
+let mongourl = "";
 
-module.exports = mongoose
+if (process.env.NODE_ENV === "production") {
+  mongourl = process.env.DB_URL;
+} else {
+  mongourl = "mongodb://127.0.0.1/buzz-kill-mern-api";
+}
+
+mongoose.connect(mongourl);
+// mongoose.connect("mongodb://localhost/vinyl-api")
+
+module.exports = mongoose;
+
+// const mongoose = require('mongoose')
+
+// mongoose.connect("mongodb://127.0.0.1/buzz-kill-mern-api")
+
+// module.exports = mongoose
